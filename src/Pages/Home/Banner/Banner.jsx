2d9a87908218292e859c9e66d5d271292133eaf5
momenur <momenurislam6@gmail.com>
2023-07-15 22:56:40 +0600

    added footer and navbar

---
+++ src/Pages/Home/Banner/Banner.jsx
@@ -0,0 +1,20 @@
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import slider1 from '../../../assets/Home/01.jpg'
+// import slider2 from '../../../assets/Home/02.jpg'
+const Banner = () => {
+    return (
+        <div>
+            <Carousel showArrows={true}>
+                <div>
+                    <img src={slider1}/>
+                </div>
+                {/* <div>
+                    <img src={slider2}/>
+                </div> */}
+            </Carousel>
+        </div>
+    );
+};
+
+export default Banner;
