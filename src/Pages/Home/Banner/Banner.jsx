94a166e6cff5dede32e4fcdc7e933b404cdd5d3f
momenur <momenurislam6@gmail.com>
2023-07-15 23:00:12 +0600

    added slider photo

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,7 +1,7 @@
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import slider1 from '../../../assets/Home/01.jpg'
-// import slider2 from '../../../assets/Home/02.jpg'
+import slider2 from '../../../assets/Home/02.jpg'
 const Banner = () => {
     return (
         <div>
@@ -9,9 +9,9 @@ const Banner = () => {
                 <div>
                     <img src={slider1}/>
                 </div>
-                {/* <div>
+                <div>
                     <img src={slider2}/>
-                </div> */}
+                </div>
             </Carousel>
         </div>
     );
