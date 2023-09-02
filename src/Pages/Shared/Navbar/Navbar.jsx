19fad893469ac72565e03b180a6584ad742dbef3
momenur <momenurislam6@gmail.com>
2023-09-02 13:37:10 +0600

    added style at navbar

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,9 +1,10 @@
 import { useContext } from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { AuthContext } from "../../../providers/AuthProvider";
 import Swal from "sweetalert2";
 import useCart from "../../../hooks/useCart";
 import { FaShoppingCart } from "react-icons/fa";
+import './Navbar.css'
 
 const Navbar = () => {
     const { user, loading, logOut } = useContext(AuthContext)
@@ -27,41 +28,42 @@ const Navbar = () => {
             })
     }
     const navItem = <>
-        <li><Link to='/'>Home</Link></li>
-        <li><Link to='/menu'>Our Menu</Link></li>
-        <li><Link to={`/order/salads`}>Order Food</Link></li>
+        <li><NavLink to='/'>Home</NavLink></li>
+        <li><NavLink to='/menu'>Our Menu</NavLink></li>
+        <li><NavLink to={`/order/salads`}>Order Food</NavLink></li>
         <li>
-            <Link to='dashboard'>
+            <NavLink to='dashboard'>
                 <FaShoppingCart /> Dashboard
                 <div className="badge badge-secondary">+{cart?.length}</div>
-            </Link>
+            </NavLink>
         </li>
         {
             user ? <>
-                <li onClick={handleLogOut}><Link to='/login'>Sign Out</Link></li>
+                <li onClick={handleLogOut}><NavLink to='/login'>Sign Out</NavLink></li>
             </> : <>
-                <li><Link to='/register'>Sign Up</Link></li>
-                <li><Link to='/login'>Login</Link></li>
+                <li><NavLink to='/register'>Sign Up</NavLink></li>
+                <li><NavLink to='/login'>Login</NavLink></li>
             </>
         }
 
     </>
-    console.log(user);
     return (
         <div>
-            <div className="fixed z-50 max-w-screen-xl text-white bg-black navbar bg-opacity-40">
+            <div className="fixed z-50 max-w-screen-xl text-white bg-black navbar bg-opacity-40" id="customActive">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
-                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 customUl">
                             {
                                 navItem
                             }
                         </ul>
                     </div>
-                    <a className="">Bistro Boss</a>
+                    <div>
+                        <a className="text-xl tracking-[2px] cursor-pointer">BISTRO BOSS<span className="block text-sm tracking-[6px] text-center uppercase">Restaurant</span></a>
+                    </div>
                 </div>
                 <div className="hidden navbar-center lg:flex">
                     <ul className="px-1 menu menu-horizontal">
@@ -71,9 +73,8 @@ const Navbar = () => {
                     </ul>
                 </div>
                 <div className="gap-4 navbar-end">
-                    <a className="font-semibold ">Hi <span className="text-sm text-yellow-500">{user?.displayName}</span></a>
                     <div className="w-10 rounded-full">
-                        <img className="w-[40px] h-[40px] rounded-full" src={user?.photoURL}/>
+                        <img className="w-[40px] h-[40px] rounded-full" src={user?.photoURL} />
                     </div>
                 </div>
             </div>
